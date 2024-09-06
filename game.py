cd0fc72e2b29a636b340ba7fb21ff9d85939a8d6
eliRschool <erosenki@oberlin.edu>
2024-09-06 15:47:17 +0000

    populate deck

--- game.py
+++ game.py
@@ -1,1 +1,31 @@
-print("test")
+#this is a simple game of open-hand blackjack intended for two players: a dealer and another player
+#Author: Eli Rosenkim, Last Edited: Sep 6 2024
+#I consulted w3schools.com for a refresher on python syntax but otherwise all work is original
+
+#define a class for the cards, containing a value and a suit
+class Card:
+    def __init__(self, value, suit):
+        self.value = value
+        self.suit = suit
+
+
+#populate the deck of cards
+dealing_shoe = [] #this a list of all cards yet to be dealt
+for value_index in range(0, 13):
+    values = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace"]
+    suits = ["spades", "hearts", "clubs", "diamonds"]
+    for suit_index in range(0, 4):
+        dealing_shoe.append(Card(values[value_index], suits[suit_index]))
+
+#DEBUGGING print all the cards in the deck
+'''
+for i in range(0, 52):
+    print(str(dealing_shoe[i].value), str(dealing_shoe[i].suit))
+'''
+
+
+#deal cards to both players
+player_1_dealer_hand = []
+player_2_hand = []
+
+
