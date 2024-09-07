d794e44c448c2cdfa359f2abcb0ed161cf61f4b2
eliRschool <erosenki@oberlin.edu>
2024-09-07 04:38:18 +0000

    game finished

--- game.py
+++ game.py
@@ -30,7 +30,7 @@ for i in range(0, 52):
     print(str(dealing_shoe[i].value), str(dealing_shoe[i].suit))
 '''
 
-#deal a random card and print the entire contents of the hand
+#deal a random card to dealer and print dealer hand
 def dealer_draw():
     player_1_dealer_hand.append(dealing_shoe.pop(random.randint(0, len(dealing_shoe)-1)))
     print("Player 1 (Dealer)'s hand is: ")
@@ -39,7 +39,8 @@ def dealer_draw():
         print(str(player_1_dealer_hand[i].value), "of", str(player_1_dealer_hand[i].suit))
         time.sleep(1.2)
 
-def player_2_draw():
+#deal random card to player 2, print player 2 hand
+def player_draw():
     player_2_hand.append(dealing_shoe.pop(random.randint(0, len(dealing_shoe)-1)))
     print("Player 2's hand is: ")
     time.sleep(1)
@@ -48,7 +49,7 @@ def player_2_draw():
         time.sleep(1.2)
 
 
-#tally the amount of points in a player's hand
+#tally the amount of points in a player's hand, given a hand list as argument
 def tally_points(player_hand):
     points = 0
     for i in range(0, len(player_hand)):
@@ -64,19 +65,20 @@ def tally_points(player_hand):
 
 
 
-#GAME STARTS HERE!!!!!!!!!!!!!!
+#########################MAIN GAME STARTS HERE########################
 
 #initial draw so that each player gets *two* cards at the start of the game
 player_1_dealer_hand.append(dealing_shoe.pop(random.randint(0, len(dealing_shoe)-1)))
 player_2_hand.append(dealing_shoe.pop(random.randint(0, len(dealing_shoe)-1)))
 dealer_draw()
-player_2_draw()
+player_draw()
 
 #player_1_dealer_hand.append(Card("King", 0)) #blackjack for debug
 #player_1_dealer_hand.append(Card("Ace", 0)) #
 #player_2_hand.append(Card("King", 0)) #blackjack for debug
 #player_2_hand.append(Card("Ace", 0)) #
 
+#tally points
 print("Tallying Player 1 (Dealer) 's points...")
 time.sleep(2)
 player_1_dealer_points = tally_points(player_1_dealer_hand)
@@ -89,7 +91,7 @@ player_2_points = tally_points(player_2_hand)
 print("Player 2's hand is worth", str(player_2_points), "points.")
 time.sleep(2)
 
-#test for blackjack
+#test for blackjack and busts
 if player_1_dealer_points == 21:
     if player_2_points ==21:
         print("Tie! both players drew blackjack.")
@@ -100,10 +102,52 @@ if player_1_dealer_points == 21:
 if player_2_points == 21:
         print("Player 2 drew blackjack. Player 2 Wins!")
         exit(1)
+if int(player_2_points) > 21:
+            print("Bust!, Player 2 Loses")
+            exit(1)
+if player_1_dealer_points > 21:
+            print("Bust!, Player 1 (Dealer) Loses")
+            exit(1)
 
-#this conditional loop contains the main game
+
+#Player's Turn
 while True:
-    
+    if input("Would you like to hit or stand? Please respond by typing 'hit' or 'stand'") == 'hit':
+        time.sleep(1)
+        player_draw()
+        time.sleep(1)
+        print("Tallying Player 2's points...")
+        time.sleep(2)
+        player_2_points = tally_points(player_2_hand)
+        print("Player 2's hand is worth", str(player_2_points), "points.")
+        time.sleep(2)
+        if int(player_2_points) > 21:
+            print("Bust!, Player 2 Loses")
+            exit(1)
+    else:
+        break
+
+#Dealer's Turn
+while player_1_dealer_points < 17:
+    dealer_draw()
+    time.sleep(1)
+    print("Tallying Player 1 (Dealer) 's points...")
+    time.sleep(2)
+    player_1_dealer_points = tally_points(player_1_dealer_hand)
+    print("Player 1 (Dealer)'s hand is worth", str(player_1_dealer_points), "points.")
+    time.sleep(2)
+    if player_1_dealer_points > 21:
+            print("Bust!, Player 1 (Dealer) Loses")
+            exit(1)
+
+#test for wins if neither player busts
+if player_1_dealer_points > player_2_points:
+     print("Player 1 Wins!")
+     exit(1)
+elif player_1_dealer_points < player_2_points:
+     print("Player 2 Wins!")
+else:
+    print("Draw")
 
     
 
